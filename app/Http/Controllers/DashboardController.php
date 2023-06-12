39b8f382b21112c0cd378b099543f055297cddb2
thecaracter <92218072+Thecaracter@users.noreply.github.com>
2023-06-12 14:52:46 +0700

    penambahanan chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,17 +7,44 @@ use App\Models\Perusahaan;
 use App\Models\Posisi;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
+
     public function index()
     {
         $userCount = User::count();
         $penyimpananCount = Penyimpanan::count();
         $perusahaanCount = Perusahaan::count();
         $posisiCount = Posisi::count();
-        return view('admin.dashboard', compact('userCount', 'penyimpananCount', 'perusahaanCount', 'posisiCount'));
+
+        // Mengambil data jumlah penyimpanan berdasarkan bulan
+        $data = Penyimpanan::select(
+            DB::raw('DATE_FORMAT(tanggal, "%Y-%m") as month'),
+            DB::raw('COUNT(*) as total')
+        )
+            ->groupBy('month')
+            ->orderBy('month')
+            ->get();
+
+        // Mengelompokkan data berdasarkan bulan
+        $chartData = [];
+        foreach ($data as $item) {
+            $month = Carbon::createFromFormat('Y-m', $item->month)->format('F Y');
+            $chartData[] = [
+                'month' => $month,
+                'total' => $item->total
+            ];
+        }
+
+        // Mengonversi data ke dalam format yang diperlukan oleh charting library (misalnya, JSON)
+        $chartDataJson = json_encode($chartData);
+
+        return view('admin.dashboard', compact('userCount', 'penyimpananCount', 'perusahaanCount', 'posisiCount', 'chartDataJson'));
     }
+
 
 
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -91,99 +91,40 @@
                 </div>
             </div>
         </div>
-        {{-- @if (Auth::user()->role == 'admin')
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="card">
-                        <div class="card-header">
-                            Grafik Pendaftaran per Bulan (Bar Chart)
-                        </div>
-                        <div class="card-body">
-                            <canvas id="barChart"></canvas>
-                        </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        Grafik Pendaftaran per Bulan (Bar Chart)
                     </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card">
-                        <div class="card-header">
-                            Grafik Pendaftaran per Bulan (Line Chart)
-                        </div>
-                        <div class="card-body">
-                            <canvas id="lineChart"></canvas>
-                        </div>
+                    <div class="card-body">
+                        <canvas id="barChart"></canvas>
                     </div>
                 </div>
             </div>
-        @endif
-        <div class="row">
-            @foreach ($lombas as $lomba)
-                <div class="col-12 col-md-6 col-lg-3">
-                    <div class="card card-info">
-                        <div class="card-header">
-                            <h4>{{ $lomba->nama }}</h4>
-                        </div>
-                        <div class="card-body d-flex align-items-center">
-                            <div class="text-center">
-                                <img src="{{ $lomba->foto }}" alt="Foto Lomba" class="img-fluid rounded mb-3 mr-3"
-                                    style="object-fit: cover; max-height: 200px;">
-                            </div>
-                            <div class="ml-3">
-                                <h6>Tanggal Mulai</h6>
-                                <p>{{ $lomba->tanggal_mulai }}</p>
-                                <h6>Tanggal Selesai</h6>
-                                <p>{{ $lomba->tanggal_selesai }}</p>
-                                <button type="button" class="btn btn-primary" data-toggle="modal"
-                                    data-target="#modal{{ $lomba->id }}">
-                                    Detail
-                                </button>
-                            </div>
-                        </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        Grafik Pendaftaran per Bulan (Line Chart)
+                    </div>
+                    <div class="card-body">
+                        <canvas id="lineChart"></canvas>
                     </div>
                 </div>
-            @endforeach
+            </div>
         </div>
     </div>
-    <!-- modal detail -->
-    @foreach ($lombas as $lomba)
-        <div class="modal fade" id="modal{{ $lomba->id }}" tabindex="-1" role="dialog"
-            aria-labelledby="modal{{ $lomba->id }}Label" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="modal{{ $lomba->id }}Label">{{ $lomba->nama }}</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="text-center">
-                            <img src="{{ $lomba->foto }}" alt="Foto Lomba" class="img-fluid rounded mb-3"
-                                style="object-fit: cover; max-height: 200px;">
-                        </div>
-                        <h6>Tanggal Mulai</h6>
-                        <p>{{ $lomba->tanggal_mulai }}</p>
-                        <h6>Tanggal Selesai</h6>
-                        <p>{{ $lomba->tanggal_selesai }}</p>
-                        <h6>Deskripsi</h6>
-                        <p>{{ $lomba->deskripsi }}</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    @endforeach
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
         var barChartCtx = document.getElementById('barChart').getContext('2d');
         var barChart = new Chart(barChartCtx, {
             type: 'bar',
             data: {
-                labels: {!! json_encode($bulanLabels) !!},
+                labels: {!! $chartDataJson !!}.map(data => data.month),
                 datasets: [{
-                    label: 'Pendaftaran',
-                    data: {!! json_encode($pendaftaranDataArray) !!},
+                    label: 'Penyimpanan',
+                    data: {!! $chartDataJson !!}.map(data => data.total),
                     backgroundColor: 'rgba(54, 162, 235, 0.5)',
                     borderColor: 'rgba(54, 162, 235, 1)',
                     borderWidth: 1
@@ -203,10 +144,10 @@
         var lineChart = new Chart(lineChartCtx, {
             type: 'line',
             data: {
-                labels: {!! json_encode($bulanLabels) !!},
+                labels: {!! $chartDataJson !!}.map(data => data.month),
                 datasets: [{
-                    label: 'Pendaftaran',
-                    data: {!! json_encode($pendaftaranDataArray) !!},
+                    label: 'Penyimpanan',
+                    data: {!! $chartDataJson !!}.map(data => data.total),
                     backgroundColor: 'rgba(54, 162, 235, 0.5)',
                     borderColor: 'rgba(54, 162, 235, 1)',
                     borderWidth: 1,
@@ -222,5 +163,5 @@
                 }
             }
         });
-    </script> --}}
-    @endsection
+    </script>
+@endsection
